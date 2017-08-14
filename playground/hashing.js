c658ccc294e29e289fbed2417ca3fe3b710d68e4
Maciej Wasilak <wasilak@gmail.com>
2017-08-14 13:44:09 +0200

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,18 +1,35 @@
-const {SHA256} = require('crypto-js');
+const bcrypt = require('bcryptjs');
 
-const jwt = require('jsonwebtoken');
+var password = "abc123!";
+var hashed;
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+    hashedPassword = hash;
 
-var data = {
-  id: 10
-};
+    bcrypt.compare(password, hashedPassword, (err, res) => {
+      console.log(res);
+    });
 
-var token = jwt.sign(data, '123abc');
+  })
+})
 
-console.log(token);
 
-var decoded = jwt.verify(token, '123abc');
-
-console.log(decoded);
+// const {SHA256} = require('crypto-js');
+//
+// const jwt = require('jsonwebtoken');
+//
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+//
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc');
+//
+// console.log(decoded);
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
